26fba4daf8de5fa33bb0cce432bb4543e42a1781
meli-imelda <meliimeldatchouala@gmail.com>
2020-09-12 03:40:14 +0100

    exercise 3

---
+++ chapter_3/chapter_3_exercises/exercise_3.py
@@ -0,0 +1,11 @@
+import turtle 
+window = turtle.Screen() 
+
+window.bgcolor("blue")
+window.title("Exercise 3")
+
+tess = turtle.Turtle()
+tess.left(3645)
+
+# After running the program, tess is now facing at 45 degrees
+# and she rotated about this position a good number of times
